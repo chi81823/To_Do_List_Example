60d37e98bf411872f2cf47c7a7c703d1998b8a92
Darren Chi <chi81823@gmail.com>
2018-11-02 00:07:46 +0800

    request.js merge

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -1,69 +1,93 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
     $("#todolistForm").submit(function(event) {
 		event.preventDefault();
-		ajaxPost();
+		var formData = {
+            name : $("#name").val(),
+            content :  $("#content").val()
+        }
+
+        $.ajax({
+            type : "POST",
+            contentType : "application/json",
+            url : window.location + "api/todolist/",
+            data : JSON.stringify(formData),
+            dataType : 'json',
+            success : function(result) {
+                console.log(result);
+            },
+            error : function(e) {
+                alert("Error!")
+                console.log("ERROR: ", e);
+            }
+        });
+        window.location.reload();
 	});
 
 	$("#todolistUpdateForm").submit(function(event) {
-    		event.preventDefault();
-    		ajaxPut();
-    });
-
-    function ajaxPut(){
-	    var formData = {
-	        id : $("#updateId").val(),
+        event.preventDefault();
+    	var formData = {
+            id : $("#updateId").val(),
             name : $("#updateName").val(),
             content : $("#updateContext").val()
         }
-        var id = $("#updateId").val();
 
+        var id = $("#updateId").val();
         console.log("formData before PUT: " + formData);
 
         $.ajax({
-        	type : "PUT",
-        	contentType : "application/json",
-        	url : window.location + "api/todolist/" + id,
-        	data : JSON.stringify(formData),
-        	dataType : 'json',
-        	success : function(result) {
+            type : "PUT",
+            contentType : "application/json",
+            url : window.location + "api/todolist/" + id,
+            data : JSON.stringify(formData),
+            dataType : 'json',
+            success : function(result) {
                 console.log(result);
-			},
-			error : function(e) {
-				alert("Error!")
-				console.log("ERROR: ", e);
-			}
-		});
-	}
+            },
+            error : function(e) {
+                alert("Error!")
+                console.log("ERROR: ", e);
+            }
+        });
+        window.location.reload();
+    });
 
-	function ajaxPost(){
-        var formData = {
-        	name : $("#name").val(),
-        	content :  $("#content").val()
-        }
+    $("#toDoListTable").on("click","a",function() {
+        var todolistId = $(this).parent().find("#deleteId").val();
 
         $.ajax({
-    		type : "POST",
-    		contentType : "application/json",
-    		url : window.location + "api/todolist/",
-    		data : JSON.stringify(formData),
-    		dataType : 'json',
-    		success : function(result) {
-    			console.log(result);
-    		},
-    		error : function(e) {
-    			alert("Error!")
-    			console.log("ERROR: ", e);
-    		}
-    	});
-
-        // Reload
-        resetData();
+            type : "DELETE",
+            url : window.location + "api/todolist/" + todolistId,
+            success: function(result){
+                console.log(result);
+            },
+            error : function(e) {
+                alert("ERROR: ", e);
+                console.log("ERROR: ", e);
+            }
+        });
         window.location.reload();
-    }
+    });
 
-    function resetData(){
-       	$("#name").val("");
-       	$("#content").val("");
-    }
+    $.ajax({
+        type : "GET",
+        url : window.location + "api/todolist/all",
+        success: function(result){
+            $.each(result, function(index, todolist){
+                var toDoListRow = '<tr>' + index +
+                                  '<td>' + todolist.id + '</td>' +
+                                  '<td>' + todolist.name + '</td>' +
+                                  '<td>' + todolist.content + '</td>' +
+                                  '<td>' +
+                                  '<input type="hidden" id="deleteId" value=' + todolist.id + '>' +
+                                  '<a>' + '<button type="submit" >Delete</button>' + '</a>' +
+                                  '</td>' + '</tr>';
+                $('#toDoListTable tbody').append(toDoListRow);
+            });
+        },
+        error : function(e) {
+            alert("ERROR: ", e);
+            console.log("ERROR: ", e);
+        }
+    });
 })
