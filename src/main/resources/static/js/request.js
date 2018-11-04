35bfb7545543d636c5be43e05fc2e63aabc78f93
Darren Chi <chi81823@gmail.com>
2018-11-04 20:28:12 +0800

    request.js Double quotation clear

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -1,77 +1,78 @@
 $(document).ready(function() {
 
-    $("#todolistForm").submit(function(event) {
+    $('#todolistForm').submit(function(event) {
 		event.preventDefault();
 		var formData = {
-            name : $("#name").val(),
-            content :  $("#content").val()
+            name : $('#name').val(),
+            content :  $('#content').val()
         }
 
         $.ajax({
-            type : "POST",
-            contentType : "application/json",
-            url : window.location + "api/todolist/",
+            type : 'POST',
+            contentType : 'application/json',
+            url : window.location + 'api/todolist/',
             data : JSON.stringify(formData),
             dataType : 'json',
             success : function(result) {
                 console.log(result);
             },
             error : function(e) {
-                alert("Error!")
-                console.log("ERROR: ", e);
+                alert('Error!')
+                console.log('ERROR: ', e);
             }
         });
+        resetData();
         window.location.reload();
 	});
 
-	$("#todolistUpdateForm").submit(function(event) {
+	$('#todolistUpdateForm').submit(function(event) {
         event.preventDefault();
     	var formData = {
-            id : $("#updateId").val(),
-            name : $("#updateName").val(),
-            content : $("#updateContext").val()
+            id : $('#updateId').val(),
+            name : $('#updateName').val(),
+            content : $('#updateContext').val()
         }
 
-        var id = $("#updateId").val();
-        console.log("formData before PUT: " + formData);
+        var id = $('#updateId').val();
+        console.log('formData before PUT: ' + formData);
 
         $.ajax({
-            type : "PUT",
-            contentType : "application/json",
-            url : window.location + "api/todolist/" + id,
+            type : 'PUT',
+            contentType : 'application/json',
+            url : window.location + 'api/todolist/' + id,
             data : JSON.stringify(formData),
             dataType : 'json',
             success : function(result) {
                 console.log(result);
             },
             error : function(e) {
-                alert("Error! 找不到此ID")
-                console.log("ERROR: ", e);
+                alert('Error! 找不到此ID')
+                console.log('ERROR: ', e);
             }
         });
         window.location.reload();
     });
 
-    $("#toDoListTable").on("click","a",function() {
-        var todolistId = $(this).parent().find("#deleteId").val();
+    $('#toDoListTable').on('click','a',function() {
+        var todolistId = $(this).parent().find('#deleteId').val();
 
         $.ajax({
-            type : "DELETE",
-            url : window.location + "api/todolist/" + todolistId,
+            type : 'DELETE',
+            url : window.location + 'api/todolist/' + todolistId,
             success: function(result){
                 console.log(result);
             },
             error : function(e) {
-                alert("ERROR: ", e);
-                console.log("ERROR: ", e);
+                alert('ERROR: ', e);
+                console.log('ERROR: ', e);
             }
         });
         window.location.reload();
     });
 
     $.ajax({
-        type : "GET",
-        url : window.location + "api/todolist/all",
+        type : 'GET',
+        url : window.location + 'api/todolist/all',
         success: function(result){
             $.each(result, function(index, todolist){
                 var toDoListRow = '<tr>' + index +
@@ -86,8 +87,13 @@ $(document).ready(function() {
             });
         },
         error : function(e) {
-            alert("ERROR: ", e);
-            console.log("ERROR: ", e);
+            alert('ERROR: ', e);
+            console.log('ERROR: ', e);
         }
     });
+
+    function resetData(){
+           	$("#name").val("");
+           	$("#content").val("");
+        }
 })
