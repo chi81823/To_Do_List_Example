b4bf64f3ceeff052386c8e4e62501bf5fc7eceb6
Darren Chi <chi81823@gmail.com>
2018-11-11 13:00:15 +0800

    add date

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('#todolistEnterForm').bind('keypress', function(event) {
-		event.preventDefault();
+    $('#todolistEnterForm').bind('keypress', function (event) {
+        event.preventDefault();
 
         var formData = {
-            name : $('#enterName').val()
+            name: $('#enterName').val()
         }
-        if(event.which == 13 || event.keyCode == 13){
+        if (event.which == 13 || event.keyCode == 13) {
 
             $.ajax({
-                type : 'POST',
-                contentType : 'application/json',
-                url : window.location + 'api/todolist/',
-                data : JSON.stringify(formData),
-                dataType : 'json',
-                success : function(result) {
+                type: 'POST',
+                contentType: 'application/json',
+                url: window.location + 'api/todolist/',
+                data: JSON.stringify(formData),
+                dataType: 'json',
+                success: function (result) {
                     console.log(result);
                 },
-                error : function(e) {
+                error: function (e) {
                     alert('Error!')
                     console.log('ERROR: ', e);
                 }
@@ -25,102 +25,115 @@ $(document).ready(function() {
             resetData();
             window.location.reload();
         }
-	});
+    });
 
-    $('#todolistForm').submit(function(event) {
-		event.preventDefault();
-		var formData = {
-            name : $('#name').val(),
-            content :  $('#content').val()
+    $('#todolistForm').submit(function (event) {
+        event.preventDefault();
+        
+        function testValid(now) {
+            var regex = /^(2)((\d{3})\/(0[13578]|1[02])\/(0[1-9]|[12]\d|3[01])|(\d{3})\/(0[469]|11)\/(0[1-9]|[12]\d|30)|(\d{3})\/(02)\/(0[1-9]|[12]\d))$/;
+            return regex.test(now);
+        }
+
+        var formData = {
+            date: $('#date').val(),
+            name: $('#name').val(),
+            content: $('#content').val()
+        }
+        
+        if (testValid(date)) {
+            $.ajax({
+                type: 'POST',
+                contentType: 'application/json',
+                url: window.location + 'api/todolist/',
+                data: JSON.stringify(formData),
+                dataType: 'json',
+                success: function (result) {
+                    console.log(result);
+                },
+                error: function (e) {
+                    alert('Error!')
+                    console.log('ERROR: ', e);
+                }
+            });
+            resetData();
+            window.location.reload();
+        } else {
+            alert('時間錯誤');
+        }
+    });
+
+
+
+    $('#noFormButton').click(function (event) {
+        event.preventDefault();
+        var formData = {
+            name: $('#name1').val(),
+            content: $('#content1').val()
         }
 
         $.ajax({
-            type : 'POST',
-            contentType : 'application/json',
-            url : window.location + 'api/todolist/',
-            data : JSON.stringify(formData),
-            dataType : 'json',
-            success : function(result) {
+            type: 'POST',
+            contentType: 'application/json',
+            url: window.location + 'api/todolist/',
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success: function (result) {
                 console.log(result);
             },
-            error : function(e) {
+            error: function (e) {
                 alert('Error!')
                 console.log('ERROR: ', e);
             }
         });
         resetData();
         window.location.reload();
-	});
+    });
 
-	$('#noFormButton').click(function(event) {
-    		event.preventDefault();
-    		var formData = {
-                name : $('#name1').val(),
-                content :  $('#content1').val()
-            }
-
-            $.ajax({
-                type : 'POST',
-                contentType : 'application/json',
-                url : window.location + 'api/todolist/',
-                data : JSON.stringify(formData),
-                dataType : 'json',
-                success : function(result) {
-                    console.log(result);
-                },
-                error : function(e) {
-                    alert('Error!')
-                    console.log('ERROR: ', e);
-                }
-            });
-            resetData();
-            window.location.reload();
-    	});
-
-	$('#todolistParamForm').submit(function(event) {
-    		event.preventDefault();
-    		var formData = {
-                name : $('#paramName').val(),
-                content :  $('#paramContent').val()
-            }
-
-            $.ajax({
-                type : 'GET',
-                url : window.location + 'api/todolist/requestParam',
-                data : 'name=' + formData.name + '&content=' + formData.content,
-                success : function(result) {
-                    console.log(result);
-                },
-                error : function(e) {
-                    alert('Error!')
-                    console.log('ERROR: ', e);
-                }
-            });
-            resetData();
-            window.location.reload();
-    	});
-
-	$('#todolistUpdateForm').submit(function(event) {
+    $('#todolistParamForm').submit(function (event) {
         event.preventDefault();
-    	var formData = {
-            id : $('#updateId').val(),
-            name : $('#updateName').val(),
-            content : $('#updateContext').val()
+        var formData = {
+            name: $('#paramName').val(),
+            content: $('#paramContent').val()
+        }
+
+        $.ajax({
+            type: 'GET',
+            url: window.location + 'api/todolist/requestParam',
+            data: 'name=' + formData.name + '&content=' + formData.content,
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (e) {
+                alert('Error!')
+                console.log('ERROR: ', e);
+            }
+        });
+        resetData();
+        window.location.reload();
+    });
+
+    $('#todolistUpdateForm').submit(function (event) {
+        event.preventDefault();
+        var formData = {
+            id: $('#updateId').val(),
+            name: $('#updateName').val(),
+            content: $('#updateContext').val()
         }
 
         var id = $('#updateId').val();
         console.log('formData before PUT: ' + formData);
 
         $.ajax({
-            type : 'PUT',
-            contentType : 'application/json',
-            url : window.location + 'api/todolist/' + id,
-            data : JSON.stringify(formData),
-            dataType : 'json',
-            success : function(result) {
+            type: 'PUT',
+            contentType: 'application/json',
+            url: window.location + 'api/todolist/' + id,
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success: function (result) {
                 console.log(result);
             },
-            error : function(e) {
+            error: function (e) {
                 alert('Error! 找不到此ID')
                 console.log('ERROR: ', e);
             }
@@ -128,16 +141,16 @@ $(document).ready(function() {
         window.location.reload();
     });
 
-    $('#toDoListTable').on('click','a',function() {
+    $('#toDoListTable').on('click', 'a', function () {
         var todolistId = $(this).parent().find('#deleteId').val();
 
         $.ajax({
-            type : 'DELETE',
-            url : window.location + 'api/todolist/' + todolistId,
-            success: function(result){
+            type: 'DELETE',
+            url: window.location + 'api/todolist/' + todolistId,
+            success: function (result) {
                 console.log(result);
             },
-            error : function(e) {
+            error: function (e) {
                 alert('ERROR: ', e);
                 console.log('ERROR: ', e);
             }
@@ -146,29 +159,29 @@ $(document).ready(function() {
     });
 
     $.ajax({
-        type : 'GET',
-        url : window.location + 'api/todolist/all',
-        success: function(result){
-            $.each(result, function(index, todolist){
+        type: 'GET',
+        url: window.location + 'api/todolist/all',
+        success: function (result) {
+            $.each(result, function (index, todolist) {
                 var toDoListRow = '<tr>' + index +
-                                  '<td>' + todolist.id + '</td>' +
-                                  '<td>' + todolist.name + '</td>' +
-                                  '<td>' + todolist.content + '</td>' +
-                                  '<td>' +
-                                  '<input type="hidden" id="deleteId" value=' + todolist.id + '>' +
-                                  '<a>' + '<button type="submit" >Delete</button>' + '</a>' +
-                                  '</td>' + '</tr>';
+                    '<td>' + todolist.id + '</td>' +
+                    '<td>' + todolist.name + '</td>' +
+                    '<td>' + todolist.content + '</td>' +
+                    '<td>' +
+                    '<input type="hidden" id="deleteId" value=' + todolist.id + '>' +
+                    '<a>' + '<button type="submit" >Delete</button>' + '</a>' +
+                    '</td>' + '</tr>';
                 $('#toDoListTable tbody').append(toDoListRow);
             });
         },
-        error : function(e) {
+        error: function (e) {
             alert('ERROR: ', e);
             console.log('ERROR: ', e);
         }
     });
 
-    function resetData(){
-           	$('#name').val();
-           	$('#content').val();
-        }
+    function resetData() {
+        $('#name').val();
+        $('#content').val();
+    }
 })
